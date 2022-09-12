b9565c46cbef326b2fbf891a36967856756bbcb2
Do Thanh <thanhcoral@gmail.com>
2022-09-12 17:07:18 +0700

    up

--- user/models.py
+++ user/models.py
@@ -5,10 +5,16 @@ from django.utils import timezone
 from django.utils.translation import gettext_lazy as _
 
 
+GENDER_CHOICES = (
+    ('Male', 'Male'),
+    ('Female', 'Female'),
+    ('Unknown', 'Unknown'),
+)
+
 class User(AbstractBaseUser, PermissionsMixin):
     username = models.CharField(_("username"), max_length=150, unique=True, validators=[UnicodeUsernameValidator()], error_messages={"unique": _("A user with that username already exists."),})
     first_name = models.CharField(_("first name"), max_length=150, blank=True)
-    last_name = models.CharField(_("last name"), max_length=150, blank=True)
+    last_name = models.CharField(_("last name"), max_length=150, blank=True)    # Surname
     email = models.EmailField(_("email address"), blank=True)
     is_staff = models.BooleanField(_("staff status"), default=False,)
     is_active = models.BooleanField(_("active"), default=True)
@@ -23,3 +29,11 @@ class User(AbstractBaseUser, PermissionsMixin):
     class Meta:
         verbose_name = _("user")
         verbose_name_plural = _("users")
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+    avatar = models.ImageField(default='avatar_default.jpg', upload_to='images')
+
+    def __str__(self):
+        return f"Profile of {self.user.username}"
