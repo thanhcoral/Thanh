6e45c98686a1fe8d3042edf4b3bc7417e9c4ada2
Do Thanh <thanhcoral@gmail.com>
2022-09-30 10:58:15 +0700

    up

--- erp/settings.py
+++ erp/settings.py
@@ -77,18 +77,18 @@ WSGI_APPLICATION = 'erp.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql', 
-        'NAME': 'erp',
-        'USER': 'root',
-        'PASSWORD': 'Viendanho@123',
-        'HOST': 'localhost',
-        'PORT': '3306',
-    }
     # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': BASE_DIR / 'db.sqlite3',
+    #     'ENGINE': 'django.db.backends.mysql', 
+    #     'NAME': 'erp',
+    #     'USER': 'root',
+    #     'PASSWORD': 'Viendanho@123',
+    #     'HOST': 'localhost',
+    #     'PORT': '3306',
     # }
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
 
 
